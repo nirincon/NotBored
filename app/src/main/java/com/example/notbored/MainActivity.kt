39d2607b8e60dc634f2fcad243833b6e65e77076
mauropena1 <mauro.pena@mercadolibre.com>
2022-06-06 15:04:18 -0300

    added navigation resource, and fragment container

--- app/src/main/java/com/example/notbored/MainActivity.kt
+++ app/src/main/java/com/example/notbored/MainActivity.kt
@@ -3,7 +3,7 @@ package com.example.notbored
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class StartActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)

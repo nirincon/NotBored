6d06e24babde510854685a69d6c23d6e1a239e23
nirincon <nikol.rincon@mercadolibre.com.co>
2022-06-06 16:44:42 -0500

    ui for activity list created

---
+++ app/src/main/java/com/example/notbored/ui/activities/ActivityFragment.kt
@@ -0,0 +1,18 @@
+package com.example.notbored.ui.activities
+
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.example.notbored.R
+
+class ActivityFragment : Fragment(R.layout.fragment_activity) {
+
+    private lateinit var binding: FragmentActivity
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}

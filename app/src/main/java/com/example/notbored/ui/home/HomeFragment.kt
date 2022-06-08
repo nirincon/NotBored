74d17e8d43b7e266dfa796706e44bc8bee0a6049
mauropena1 <mauro.pena@mercadolibre.com>
2022-06-08 14:18:01 -0300

    added dropdownmenu for select price

--- app/src/main/java/com/example/notbored/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/notbored/ui/home/HomeFragment.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import android.widget.Toast
 import androidx.core.widget.doAfterTextChanged
 import androidx.fragment.app.viewModels
@@ -18,22 +20,33 @@ import com.example.notbored.presentation.ActivitiesViewModelFactory
 import com.google.android.material.snackbar.Snackbar
 
 
-class HomeFragment : Fragment(R.layout.fragment_home) {
+class HomeFragment : Fragment(R.layout.fragment_home), AdapterView.OnItemClickListener {
     private lateinit var binding: FragmentHomeBinding
-    private val viewModel by viewModels<ActivitiesViewModel>{
+    private val viewModel by viewModels<ActivitiesViewModel> {
         ActivitiesViewModelFactory(
             ActivityRepositoryImpl(
                 ActivitiesDataSource(RetrofitClient.apiservice)
             )
         )
-
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = FragmentHomeBinding.bind(view)
         init()
+        setPreferences()
+    }
 
+
+    private fun init() {
+        binding.tvConditions.setOnClickListener { goToTerms() }
+        textListener()
+        startListener()
+        setPriceOptions()
+    }
+
+
+    private fun setPreferences() {
         val sharedPreferences = activity?.getSharedPreferences(
             getString(R.string.shared_prefs),
             Context.MODE_PRIVATE
@@ -41,32 +54,64 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
         if (sharedPreferences != null) {
             viewModel.setSharedPreferences(sharedPreferences)
         }
-
     }
 
-    private fun init(){
-        with(binding) {
-            tvConditions.setOnClickListener { goToTerms() }
-            etParticipants.doAfterTextChanged {
-                val text = etParticipants.text.toString()
-                if (text != "0") {
-                    btnStart.isEnabled = true
-                } else {
-                    btnStart.isEnabled = false
-                    etParticipants.error = "The number has to be greater than 0."
-                }
-            }
-            btnStart.setOnClickListener {
-                if (cbAceptar.isChecked){
-                    viewModel.addParticipants(etParticipants.text.toString())
-                    val action = HomeFragmentDirections.actionHomeFragmentToActivityFragment(
-                        etParticipants.text.toString(),"",""
-                    )
-                    findNavController().navigate(action)
-                }else
+    private fun setPriceOptions() {
+        val priceOptions = resources.getStringArray(R.array.price_items)
+        val adapter = ArrayAdapter(this.requireContext(), R.layout.item_list, priceOptions)
+        binding.faqSelection.setAdapter(adapter)
+        binding.faqSelection.onItemClickListener = this
+    }
+
+    private fun startListener() {
+        binding.btnStart.setOnClickListener {
+            if (binding.cbAceptar.isChecked) {
+                viewModel.addParticipants(binding.etParticipants.text.toString())
+                findNavController().navigate(R.id.action_homeFragment_to_activityFragment)
+            } else
                 Snackbar
-                    .make(binding.root,getString(R.string.accept_terms_and_conditions), Snackbar.LENGTH_SHORT)
+                    .make(
+                        binding.root,
+                        getString(R.string.accept_terms_and_conditions),
+                        Snackbar.LENGTH_SHORT
+                    )
                     .show()
+        }
+    }
+
+    private fun setValue(string: String) {
+        return when (string) {
+            in "Free" -> {
+                viewModel.addMax("0")
+                viewModel.addMin("0")
+            }
+            in "Low" -> {
+                viewModel.addMax("0.3")
+                viewModel.addMin("0")
+            }
+            in "Medium" -> {
+                viewModel.addMax("0.6")
+                viewModel.addMin("0.3")
+            }
+            in "High" -> {
+                viewModel.addMax("1")
+                viewModel.addMin("0.6")
+            }
+            else -> {
+                viewModel.addMax("")
+                viewModel.addMin("")
+            }
+        }
+    }
+
+    private fun textListener() {
+        binding.etParticipants.doAfterTextChanged {
+            val text = binding.etParticipants.text.toString()
+            if (text != "0") {
+                binding.btnStart.isEnabled = true
+            } else {
+                binding.btnStart.isEnabled = false
+                binding.etParticipants.error = "The number has to be greater than 0."
             }
         }
     }
@@ -74,5 +119,9 @@ class HomeFragment : Fragment(R.layout.fragment_home) {
     private fun goToTerms() {
         findNavController().navigate(R.id.action_homeFragment_to_termsFragment)
     }
-}
 
+    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        val item = parent?.getItemAtPosition(position).toString()
+        setValue(item)
+    }
+}
